058d30350cd0785a17c32870e32a228f2c942767
shahidmonowarr <shahidmonowar21@gmail.com>
2022-11-07 19:54:04 +0600

    conditional sign out implemented

--- src/components/shares/Header/Header.js
+++ src/components/shares/Header/Header.js
@@ -1,4 +1,5 @@
 import MenuIcon from "@mui/icons-material/Menu";
+import { Button } from "@mui/material";
 import AppBar from "@mui/material/AppBar";
 import Avatar from "@mui/material/Avatar";
 import Box from "@mui/material/Box";
@@ -9,14 +10,22 @@ import MenuItem from "@mui/material/MenuItem";
 import Toolbar from "@mui/material/Toolbar";
 import Tooltip from "@mui/material/Tooltip";
 import Typography from "@mui/material/Typography";
+import { signOut } from "firebase/auth";
 import * as React from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
 import { NavLink } from "react-router-dom";
+import auth from "../../../firebase.init";
 
 const settings = ["Dashboard", "Logout"];
 
 const Header = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [user, loading, error] = useAuthState(auth);
+
+  const logOut = () => {
+    signOut(auth);
+  };
 
   const handleOpenNavMenu = (event) => {
     setAnchorElNav(event.currentTarget);
@@ -134,49 +143,131 @@ const Header = () => {
                 />
               </NavLink>
             </Typography>
-            <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex", justifyContent: "flex-end"}, marginRight: '50px'}}>
-            <NavLink style={{ textDecoration: "none" }} to="/">
-              <Box style={{ color: "black", marginRight: 30, textTransform: 'capitalize', fontSize: "17px"}}>Home</Box>
-            </NavLink>
+            <Box
+              sx={{
+                flexGrow: 1,
+                display: { xs: "none", md: "flex", justifyContent: "flex-end" },
+                marginRight: "50px",
+              }}
+            >
+              <NavLink style={{ textDecoration: "none" }} to="/">
+                <Box
+                  style={{
+                    color: "black",
+                    marginRight: 30,
+                    textTransform: "capitalize",
+                    fontSize: "17px",
+                  }}
+                >
+                  Home
+                </Box>
+              </NavLink>
 
-            <NavLink style={{ textDecoration: "none" }} to="/services">
-              <Box style={{ color: "black",  marginRight: 30, textTransform: 'capitalize', fontSize: "17px"}}>Services</Box>
-            </NavLink>
+              <NavLink style={{ textDecoration: "none" }} to="/services">
+                <Box
+                  style={{
+                    color: "black",
+                    marginRight: 30,
+                    textTransform: "capitalize",
+                    fontSize: "17px",
+                  }}
+                >
+                  Services
+                </Box>
+              </NavLink>
 
-            <NavLink style={{ textDecoration: "none" }} to="/contact-us">
-              <Box style={{ color: "black" , textTransform: 'capitalize', fontSize: "17px"}}>Contact Us</Box>
-            </NavLink>
-          </Box>
-
-            <Box  sx={{ flexGrow: 0, marginRight: {lg: '100px'}}}>
-              <Tooltip title="Open settings">
-                <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: "45px" }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: "top",
-                  horizontal: "right",
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                {settings.map((setting) => (
-                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">{setting}</Typography>
-                  </MenuItem>
-                ))}
-              </Menu>
+              <NavLink style={{ textDecoration: "none" }} to="/contact-us">
+                <Box
+                  style={{
+                    color: "black",
+                    textTransform: "capitalize",
+                    fontSize: "17px",
+                  }}
+                >
+                  Contact Us
+                </Box>
+              </NavLink>
             </Box>
+
+            {user ? (
+              <Box sx={{ flexGrow: 0, marginRight: { lg: "100px" } }}>
+                <Tooltip title={user?.displayName}>
+                  <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                    <Avatar
+                      alt={user?.displayName}
+                      src="/static/images/avatar/2.jpg"
+                    />
+                  </IconButton>
+                </Tooltip>
+                <Menu
+                  sx={{ mt: "45px" }}
+                  id="menu-appbar"
+                  anchorEl={anchorElUser}
+                  anchorOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right",
+                  }}
+                  open={Boolean(anchorElUser)}
+                  onClose={handleCloseUserMenu}
+                >
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    <NavLink style={{ textDecoration: "none" }} to="/">
+                      <Typography style={{ color: "black" }} textAlign="center">
+                        Dashboard
+                      </Typography>
+                    </NavLink>
+                  </MenuItem>
+                  <MenuItem onClick={handleCloseUserMenu}>
+                    {user.email ? (
+                      <Box style={{ textDecoration: "none" }}>
+                        <Typography
+                          style={{ color: "black" }}
+                          onClick={logOut}
+                          textAlign="center"
+                        >
+                          Logout
+                        </Typography>
+                      </Box>
+                    ) : (
+                      <NavLink style={{ textDecoration: "none" }} to="/login">
+                        <Typography
+                          style={{ color: "black" }}
+                          textAlign="center"
+                        >
+                          Login
+                        </Typography>
+                      </NavLink>
+                    )}
+                  </MenuItem>
+                </Menu>
+              </Box>
+            ) : (
+              <NavLink style={{ textDecoration: "none" }} to="/login">
+                <Button
+                  sx={{
+                    flexGrow: 0,
+                    marginRight: { lg: "100px" },
+                    backgroundColor: "black",
+                    ":hover": { backgroundColor: "black" },
+                    py: 1,
+                    px: 3,
+                  }}
+                  variant="contained"
+                >
+                  <Typography
+                    style={{ color: "white", textTransform: "capitalize" }}
+                    textAlign="center"
+                  >
+                    Login
+                  </Typography>
+                </Button>
+              </NavLink>
+            )}
           </Toolbar>
         </Container>
       </AppBar>
